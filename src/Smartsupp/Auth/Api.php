7478b1e290be1d97f546b8f8b125e4712892fe7f
Marek Gach <gach@kurzor.net>
2019-09-24 12:25:23 +0200

    Fix PSR-2 warnings

--- src/Smartsupp/Auth/Api.php
+++ src/Smartsupp/Auth/Api.php
@@ -131,11 +131,12 @@ class Api
      *
      * @return string|null
      */
-    function getUserIpAddr(){
-        if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    private function getUserIpAddr()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             // ip from share internet
             return $_SERVER['HTTP_CLIENT_IP'];
-        } elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             // ip pass from proxy
             return $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else {
